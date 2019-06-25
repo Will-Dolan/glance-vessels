c3e31f69e535ef3ebe6f54337137a4d6a655b9ac
Will-Dolan <51340612+Will-Dolan@users.noreply.github.com>
2019-06-25 12:20:24 -0400

    Add files via upload

--- server/segment.py
+++ server/segment.py
@@ -100,7 +100,87 @@ class SegmentApi(Api):
         if self.segmenter is None:
             raise Exception('segment image is not set')
 
-        tube = self.segmenter.ExtractTube(position, self.next_tube_id, True)
+        # New Stuff
+        LowerThreshold = 10
+        UpperThreshold = 300
+        PixelType = itk.F
+        ImageType = itk.Image[PixelType,3]
+        ImageType2 = itk.Image[PixelType,2]
+
+        OldImageType = type(self.input_image)
+        ImageType = itk.Image[itk.F, 3]
+        img = itk.CastImageFilter[OldImageType, ImageType].New()(self.input_image)
+        imgRegion = img.GetLargestPossibleRegion()
+        indx = imgRegion.GetIndex()
+        indx[2] = position[2]
+        size = imgRegion.GetSize()
+        size[2] = 0
+        imgRegionCut = imgRegion
+        imgRegionCut.SetIndex(indx)
+        imgRegionCut.SetSize(size)
+        extractFilter = itk.ExtractImageFilter[ImageType, ImageType2].New()
+        extractFilter.SetInput(img)
+        extractFilter.SetDirectionCollapseToSubmatrix()
+        extractFilter.SetExtractionRegion(imgRegionCut)
+        extractFilter.Update()
+        img2 = extractFilter.GetOutput()
+        connected = itk.ConnectedThresholdImageFilter.New(img2)
+        connected.SetLower(LowerThreshold)
+        connected.SetUpper(UpperThreshold)
+        connected.SetReplaceValue(4096)
+
+        coord2 = itk.Index[2]()
+        coord2[0] = position[0]
+        coord2[1] = position[1]
+        connected.AddSeed(coord2)
+        connected.Update()
+        mask = connected.GetOutput()
+
+        invert = itk.InvertIntensityImageFilter.New(mask)
+        invert.SetMaximum(4096)
+        invert.Update()
+        maskI = invert.GetOutput()
+
+        distance = itk.DanielssonDistanceMapImageFilter.New(maskI)
+        distance.InputIsBinaryOn()
+        distance.Update()
+        dist = distance.GetOutput()
+
+        minmax = itk.MinimumMaximumImageCalculator.New(dist)
+        minmax.Compute()
+        radius = minmax.GetMaximum()
+        print('Maximally inscribed circle radius in index = ' + str(radius))
+        radius = radius * img.GetSpacing()[0]
+        print('Maximally inscribed circle radius in point = ' + str(radius))
+        radiusIndex = minmax.GetIndexOfMaximum()
+        radiusPoint = itk.Point[itk.D, 3]()
+        radiusPoint[0] = radiusIndex[0] * img.GetSpacing()[0]
+        radiusPoint[1] = radiusIndex[1] * img.GetSpacing()[1]
+        radiusPoint[2] = position[2] * img.GetSpacing()[2]
+        print('Maximally inscribed circle index = ' + str(radiusIndex))
+        print('Maximally inscribed circle point = ' + str(radiusPoint))
+        tube = itk.TubeSpatialObject[3].New()
+        tubePoint = itk.TubeSpatialObject[3].New()
+        tubePoint.SetRadiusInObjectSpace(radius)
+        tubePoint.SetPositionInObjectSpace(radiusPoint)
+        radiusPoint[0] = radiusPoint[0]+3
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        radiusPoint[0] = radiusPoint[0]-1
+        tube.AddPoint(tubePoint)
+        print(tube.GetNumberOfPoints())
+
+        # Old Stuff
+        #tube = self.segmenter.ExtractTube(position, self.next_tube_id, True)
         if tube is not None:
             self.segmenter.AddTube(tube)
             self.tube_id_mapping[tube.GetId()] = tube
